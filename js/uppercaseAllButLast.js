a7bb0cd264c09d2c31fd0691fd4b9e71dd61e783
William Stephens <william.a.stephens17@gmail.com>
2019-12-01 20:16:07 -0600

    .

--- js/uppercaseAllButLast.js
+++ js/uppercaseAllButLast.js
@@ -1,2 +1,3 @@
-function alienLanguage(str){
-    return str.split(" ").map(x => x.slice(0, -1).toUpperCase() + x.slice(-1).toLowerCase()).join(" ");
+function alienLanguage(str) {
+    return str.split(" ").map(x = > x.slice(0, -1).toUpperCase() + x.slice(-1).toLowerCase()
+}
