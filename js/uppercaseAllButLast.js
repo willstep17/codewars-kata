83a82e3aae489c2ab70004bf94e90184b8bfee3e
William Stephens <william.a.stephens17@gmail.com>
2019-12-15 21:40:22 -0600

    removeTime

--- js/uppercaseAllButLast.js
+++ js/uppercaseAllButLast.js
@@ -1,3 +1,3 @@
 function alienLanguage(str) {
-    return str.split(" ").map(x = > x.slice(0, -1).toUpperCase() + x.slice(-1).toLowerCase()
+    return str.split(" ").map(x => x.slice(0, -1).toUpperCase() + x.slice(-1).toLowerCase()
 }
